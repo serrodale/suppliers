75481901615aa3208811eb3368967d99478c0cad
serrodale <serrodale@gmail.com>
2021-02-09 15:10:01 +0100

    fix(ui): imports semantic-ui CSS

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import './index.css';
+import 'semantic-ui-css/semantic.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
